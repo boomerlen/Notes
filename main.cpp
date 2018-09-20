eaba178ec17481a905b4b83ca7397dcdb705085c
boomerlen <boomerlen@users.noreply.github.com>
2018-09-20 20:42:43 +1000

    Addition of main cpp file
    Addition of main file

---
+++ main.cpp
@@ -0,0 +1,5 @@
+/*  Main.cpp
+*   Main file for the project
+*   You know like has the main window in it and all the other stuff
+*   Will probably dump all the libraries needed for compilation/linkage in the one project cuz how else you meant to do it
+*/
