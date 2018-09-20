fb3dcb193af6de21515c2231b23a6723b3915bac
boomerlen <boomerlen@users.noreply.github.com>
2018-09-20 20:45:23 +1000

    Added include iostream

--- main.cpp
+++ main.cpp
@@ -3,3 +3,6 @@
 *   You know like has the main window in it and all the other stuff
 *   Will probably dump all the libraries needed for compilation/linkage in the one project cuz how else you meant to do it
 */
+
+
+<include> iostream
