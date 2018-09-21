11172e485d50f4245f427017abb7966c91024cb2
Hugo Sebesta <bunnieweirdo@gmail.com>
2018-09-21 11:48:08 +1000

    Testing base Note class.

--- main.cpp
+++ main.cpp
@@ -3,3 +3,21 @@
 *   Project primarily for personal use so probably no one will ever see this
 *   If anyone does then I said hi!
 */
+
+#include <iostream>
+#include "note.h"
+#include "note.cpp"
+#include "subject.h"
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!" << endl;
+  note TextFile;
+  TextFile.setName("TextFile");
+
+  cout << TextFile.getName() << endl;
+  return 0;
+
+}
