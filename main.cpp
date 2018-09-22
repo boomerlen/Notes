75207bb39a8a8f2e889feaa69e5c95b448ea0511
boomerlen <boomerlen@users.noreply.github.com>
2018-09-22 11:43:18 +1000

    Added function (UNTESTED) for creating note objects from a file

--- main.cpp
+++ main.cpp
@@ -8,10 +8,12 @@
 #include <fstream>
 #include <map>
 #include <string>
+#include <sstream>
 #include "note.h"
 #include "subject.h"
 
 #define TAG_ADDR "C:\\Users\\Hugo Sebesta\\Documents\\GitHub\\Notes\\storage\\tags.txt"
+#define NOTE_OUTLINE_ADDR "C:\\Users\\Hugo Sebesta\\Documents\\GitHub\\Notes\\storage\\notes.txt"
 
 using namespace std;
 
@@ -45,6 +47,61 @@ bool tagAdd(string tag) // Function for adding a tag to the tag file.
   file << tag;
   file.close();
   return true;
+}
+
+vector<note> initNotesVector()
+{
+  vector<notes> tempNoteVector;
+
+  ifstream file;
+  file.open(NOTE_OUTLINE_ADDR, ios::in);
+  if(!file.is_open)
+  {
+    cout << "Could not open " << NOTE_OUTLINE_ADDR << endl;
+    return tempNoteVector;
+  }
+  string buffer;
+  int count = 0;
+  while(getline(file, buffer, ";"))
+  {
+    if(count == 0)
+    {
+      // Beginning of line: Note name and init the object
+      note TempNote;
+      TempNote.setName(buffer);
+    }
+    if(count == 1)
+    {
+      // Address
+      TempNote.setLocation(buffer);
+    }
+    if(count == 2)
+    {
+      // Subjects
+      stringstream str(buffer);
+      int x = 0;
+      str >> x;
+      subjects sub = static_cast<subjects>(x);
+      TempNote.setSubject(sub);
+    }
+    if(count == 3)
+    {
+      // TAGS -- NOTE THIS IS MESSY - PROBABLY MANY BUGS HERE
+      string tempStr = buffer;
+      char buf;
+      while(tempStr.size > 1)
+      {
+        size_t lastComma = tempStr.find_last_of(",");
+        tempStr.copy(buf, 1, lastComma+1);
+        tempStr = tempStr.substring(0, lastComma-1);
+        TempNote.addTag(int(buf));
+      }
+      TempNote.addTag(int(buf));
+      tempNoteVector.push_back(TempNote);
+      count = 0;
+    }
+  }
+  return tempNoteVector;
 }
 
 int main()
--- subject.h
+++ subject.h
@@ -1,22 +1,22 @@
 #ifndef subject_H
 #define subject_H
 enum subjects{
-  S_MATHS_HL,
-  S_ENG_LIT_HL,
-  S_PHY_HL,
-  S_PHIL_SL,
-  S_MUS_HL,
-  S_FRE_SL,
-  S_MATHS,
-  S_ENG,
-  S_HIS,
-  S_PDH,
-  S_FRE,
-  S_ENG_ELECT,
-  S_CD,
-  S_MUSIC,
-  S_MISC,
-  S_UNDEFINED
+  S_MATHS_HL, // 0
+  S_ENG_LIT_HL, // 1
+  S_PHY_HL, // 2
+  S_PHIL_SL, // 3
+  S_MUS_HL, // 4
+  S_FRE_SL, // 5
+  S_MATHS, // 6
+  S_ENG, // 7
+  S_HIS, // 8
+  S_PDH, // 9
+  S_FRE, // 10
+  S_ENG_ELECT, // 11
+  S_CD, // 12
+  S_MUSIC, // 13
+  S_MISC, // 14
+  S_UNDEFINED // 15
 };
 
 
