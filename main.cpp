413f2543300f3d34ccbdeea87419174dc91ec8ad
boomerlen <boomerlen@users.noreply.github.com>
2018-09-22 10:42:41 +1000

    Added functions for managing tag database (file)

--- main.cpp
+++ main.cpp
@@ -5,18 +5,57 @@
 */
 
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
 #include "note.h"
 #include "subject.h"
 
+#define TAG_ADDR "storage/tags.txt"
+
 using namespace std;
+
+map<int, string> initTagDef() // Function for reading from a tag file
+{
+  ifstream file;
+  file.open(TAG_ADDR, ios::in);
+  map<int, string> mapTemp;
+  if(!file.is_open())
+  {
+    cout << "Error opening " << TAG_ADDR << endl;
+    return;
+  }
+  string line;
+  int count = 0; // NOTE THAT TAGS START AT 0 THOUGH MAP SIZES AT 1
+  while(getline(file, line))
+  {
+    map[count] = line;
+    count++;
+  }
+  file.close();
+  return mapTemp;
+}
+
+bool tagAdd(string tag) // Function for adding a tag to the tag file.
+{
+  ofstream file;
+  file.open(TAG_ADDR, ios::out | ios::app);
+  if(!file.is_open())
+    return false;
+  file << tag;
+  file.close();
+  return true;
+}
 
 int main()
 {
   cout << "Hello World!" << endl;
-  note TextFile;
-  TextFile.setName("TextFile");
 
-  cout << TextFile.getName() << endl;
+  map<int, string> tagDef = initTagDef();
+  for(int i = 0; i < (tagDef.size() - 1); i++)
+  {
+    cout << tagDef[i] << endl;
+  }
   return 0;
 
 }
