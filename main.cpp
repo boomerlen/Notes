e3905619b16d21e4104e36955f369694b2142207
Hugo Sebesta <bunnieweirdo@gmail.com>
2018-09-24 08:39:18 +1000

    Cleaned file main.cpp from merge

--- main.cpp
+++ main.cpp
@@ -8,15 +8,122 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <map>
+#include <sstream>
+
 #include "note.h"
-#include "note.cpp"
 #include "subject.h"
 
+#define TAG_ADDR "C:\\Users\\Hugo Sebesta\\Documents\\GitHub\\Notes\\storage\\tags.txt"
+#define NOTE_OUTLINE_ADDR "C:\\Users\\Hugo Sebesta\\Documents\\GitHub\\Notes\\storage\\notes.txt"
+
 using namespace std;
+
+map<int, string> initTagDef() // Function for reading from a tag file
+{
+  ifstream file;
+  file.open(TAG_ADDR, ios::in);
+  map<int, string> mapTemp;
+  if(!file.is_open())
+  {
+    cout << "Error opening " << TAG_ADDR << endl;
+    return mapTemp;
+  }
+  string line;
+  int count = 0; // NOTE THAT TAGS START AT 0 THOUGH MAP SIZES AT 1
+  while(getline(file, line))
+  {
+    mapTemp[count] = line;
+    count++;
+  }
+  file.close();
+  return mapTemp;
+}
+
+bool tagAdd(string tag) // Function for adding a tag to the tag file.
+{
+  ofstream file;
+  file.open(TAG_ADDR, ios::out | ios::app);
+  if(!file.is_open())
+    return false;
+  file << tag;
+  file.close();
+  return true;
+}
+
+vector<note> initNotesVector()
+{
+  vector<note> tempNoteVector;
+
+  ifstream file;
+  file.open(NOTE_OUTLINE_ADDR, ios::in);
+  if(!file.is_open())
+  {
+    cout << "Could not open " << NOTE_OUTLINE_ADDR << endl;
+    return tempNoteVector;
+  }
+  string buffer;
+  int count = 0;
+  note TempNote;
+  while(getline(file, buffer, ';'))
+  {
+    if(count == 0)
+    {
+      // Beginning of line: Note name and init the object
+      TempNote.setName(buffer);
+      count++;
+    }
+    else if(count == 1)
+    {
+      // Address
+      TempNote.setLocation(buffer);
+      count++;
+    }
+    else if(count == 2)
+    {
+      // Subjects
+      stringstream str(buffer);
+      int x = 0;
+      str >> x;
+      subjects sub = static_cast<subjects>(x);
+      TempNote.setSubject(sub);
+      count++;
+    }
+    else if(count == 3)
+    {
+      // TAGS - BUG, only last tag is recorded from file
+      string tempStr = buffer;
+      char buf[2];
+      while(tempStr.size() > 1)
+      {
+        size_t lastComma = tempStr.rfind(",");
+        tempStr.copy(buf, 1, lastComma+1);
+        tempStr = tempStr.substr(0, lastComma);
+        TempNote.addTag(stoi(buf));
+      }
+      TempNote.addTag(stoi(tempStr));
+
+      tempNoteVector.push_back(TempNote);
+      count = 0;
+    }
+  }
+  return tempNoteVector;
+}
 
 int main()
 {
   cout << "Hello World!" << endl;
+  // Init and debug
+  map<int, string> tagDef = initTagDef();
+  if(tagDef.empty())
+    return 1;
+
+  vector<note> noteList = initNotesVector();
+  if(noteList.empty())
+  {
+    cout << "Notelist empty" << endl;
+    return 1;
+  }
 
   return 0;
 
