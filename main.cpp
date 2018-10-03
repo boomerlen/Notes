e6c01ad0c6981b898394fe9bd5545647e59f79b7
Hugo Sebesta <bunnieweirdo@gmail.com>
2018-10-03 15:55:09 +1000

    Made better identifier

--- main.cpp
+++ main.cpp
@@ -237,7 +237,7 @@ int main(int argc, char *argv[])
 GtkApplication *app;
 int status;
 
-app = gtk_application_new("me.eme.em", G_APPLICATION_FLAGS_NONE);
+app = gtk_application_new("com.github.boomerlen.notes", G_APPLICATION_FLAGS_NONE);
 g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 status = g_application_run(G_APPLICATION(app), argc, argv);
 g_object_unref(app);
